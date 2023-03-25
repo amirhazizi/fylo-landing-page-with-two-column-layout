a5ad9ce590c37d3e3ed00f185cde89f6e730451f
amir <amirazizihosein@gmail.com>
2023-03-25 11:26:20 +0430

    update js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 // vars
-const forms = document.querySelectorAll(".form")
+const introForm = document.querySelector(".intro-form")
+const introInput = introForm.querySelector("#intro-input")
+const introLabel = introForm.querySelector("label p")
+const sectionForm = document.querySelector(".section-form")
+const sectionInput = sectionForm.querySelector("#section-input")
+const sectionLabel = sectionForm.querySelector("label p")
+// email valid format var
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-forms.forEach((form) => {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault()
-  })
-})
+introForm.addEventListener("submit", (e) =>
+  handleSubmit(e, introInput, introLabel)
+)
+sectionForm.addEventListener("submit", (e) =>
+  handleSubmit(e, sectionInput, sectionLabel)
+)
+// document.body.style.outlineColor
+const handleSubmit = (e, input, label) => {
+  e.preventDefault()
+  const value = input.value
+  console.log(value)
+  if (!value) {
+    label.textContent = "Please enter your email address"
+    input.style.borderColor = "red"
+  } else if (!value.match(mailformat)) {
+    label.textContent = "Please check your email"
+    input.style.borderColor = "red"
+  } else {
+    input.textContent = ""
+    label.textContent = ""
+    input.style.borderColor = "black"
+  }
+}
